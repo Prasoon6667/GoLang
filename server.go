4d2353cb14cdc9f98f0edc81f2755d74a1ffb808
QuizNow <wothka@gmail.com>
2018-04-19 23:32:16 +0530

    Learning basics of go web app

---
+++ server.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+		"log"
+		"net/http"
+)
+
+func handler(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain")
+	w.Write([]byte("This is an example server"))
+}
+
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Printf("About to listen on 10443. Go to https://127.0.0.1:10443/")
+	err := http.ListenAndServe(":10443", nil)
+	log.Fatal(err)
+}
